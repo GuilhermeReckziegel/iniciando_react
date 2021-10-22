730665284fd4cc28f29438811045e1d38ea73797
GuilhermeReckziegel <78482430+GuilhermeReckziegel@users.noreply.github.com>
2021-10-21 23:58:37 -0300

    atualizando versao

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  // let message = 'Me livrei da maldição !!';
+  const [message, setMessage] = useState('Me livrei da maldição !!');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">{message}</div>
+      <button onClick={() => setMessage("Hello World !!")}>mudar</button>
+    </>
   );
+     
 }
 
 export default App;
