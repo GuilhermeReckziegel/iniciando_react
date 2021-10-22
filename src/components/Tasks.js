45a5bed18f0aeab04da25e11935df7e36e1a5850
GuilhermeReckziegel <78482430+GuilhermeReckziegel@users.noreply.github.com>
2021-10-22 00:44:54 -0300

    atualizando versao

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const Tasks = (props) => {
-    console.log(props);
-    return <h1>Tasks</h1>
+import Item_task from './itemTask';
+
+const Tasks = ({ tasks }) => {
+
+    return (
+        <>
+
+        {tasks.map((item_task) => (
+            <Item_task  item_task={item_task} />
+
+        ))}
+
+        </>
+    );
 };
 
 export default Tasks;
---
+++ src/components/itemTask.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const item_task = ({item_task}) => {
+    return <h1>{item_task.title}</h1>;
+};
+ 
+export default item_task;
